9d40b650cc8313fe56d4398518e9e891050414cd
syahba <khalisasyahba@gmail.com>
2019-08-27 21:40:47 +0700

    form n amount

--- app/src/main/java/com/example/myapplication/Amount.java
+++ app/src/main/java/com/example/myapplication/Amount.java
@@ -1,14 +1,36 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Amount extends AppCompatActivity {
 
+    Button book;
+    TextView details, details_content, amount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amount);
+
+        book = findViewById(R.id.book);
+        details = findViewById(R.id.details);
+        details_content = findViewById(R.id.details_content);
+        amount = findViewById(R.id.amount);
+
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Amount.this, Searching.class);
+                startActivity(i);
+            }
+        });
+
+        
     }
 }
--- app/src/main/java/com/example/myapplication/Form.java
+++ app/src/main/java/com/example/myapplication/Form.java
@@ -1,15 +1,19 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Form extends AppCompatActivity {
 
     Button confirm;
+    TextView address, address_name, types, weight, plastic, paper, oil, plastic_amount, paper_amount, oil_amount;
+    EditText plastic_number, paper_number, oil_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,24 @@ public class Form extends AppCompatActivity {
         setContentView(R.layout.activity_form);
 
         confirm = findViewById(R.id.confirm);
+        address = findViewById(R.id.address);
+        address_name = findViewById(R.id.address_name);
+        types = findViewById(R.id.types);
+        weight = findViewById(R.id.weight);
+        plastic = findViewById(R.id.plastic);
+        plastic_amount = findViewById(R.id.plastic_amount);
+        plastic_number = findViewById(R.id.plastic_number);
+        paper = findViewById(R.id.paper);
+        paper_amount = findViewById(R.id.paper_amount);
+        paper_number = findViewById(R.id.paper_number);
+        oil = findViewById(R.id.oil);
+        oil_amount = findViewById(R.id.oil_amount);
+        oil_number = findViewById(R.id.oil_number);
 
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Form.this, Searching.class);
+                Intent i = new Intent(Form.this, Amount.class);
                 startActivity(i);
             }
         });
