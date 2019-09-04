b9ea04f8723d129f3c11174a7209f003a3621b0a
syahba <ksyaahba@gmail.com>
2019-09-04 18:46:06 +0700

    gps

--- app/src/main/java/com/example/myapplication/Waiting.java
+++ app/src/main/java/com/example/myapplication/Waiting.java
@@ -1,11 +1,11 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Waiting extends AppCompatActivity {
 
@@ -15,6 +15,8 @@ public class Waiting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting);
+
+        next = findViewById(R.id.next);
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
